cb68cc1ddf37c863cda38ecb202cb6478247cdd1
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-10-26 16:12:05 +0100

    Modified matrix test function

--- tests/test_matrix_multiplication.py
+++ tests/test_matrix_multiplication.py
@@ -3,7 +3,9 @@ import numpy as np
 
 def test_matrix_multiplication_fn(multiplication_function):
     """Test the correctness of a matrix multiplication algorithm"""
-    sizes = [i for i in range(3, 100, 7)]
+    # sizes = [i for i in range(2, 20)]
+    sizes = [i for i in range(5, 30,1)]
+    print(f"testing {multiplication_function.__name__}")
 
     for n in sizes:
         print(f"\n=== Test for {n}x{n} matrix ===")
@@ -11,10 +13,11 @@ def test_matrix_multiplication_fn(multiplication_function):
         A = np.random.rand(n, n)
         B = np.random.rand(n, n)
 
-        C_binet = multiplication_function(A, B)
+        C, flops = multiplication_function(A, B)
         C_np = np.dot(A, B)
 
-        if np.allclose(C_binet, C_np):
+        if np.allclose(C, C_np):
             print("✅ Correct result (matches numpy.dot)")
+            print(f"Flops: {flops}")
         else:
             print("❌ Incorrect result (does not match numpy.dot!)")
